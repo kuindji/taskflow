660ce0feef64e27ed36441467ff19615470774c7
Ivan Kuindzhi <kuindji@gmail.com>
2026-03-12 10:21:47 +0400

    Add layout types to AppSettings

--- packages/shared/src/types/settings.ts
+++ packages/shared/src/types/settings.ts
@@ -13,14 +13,38 @@ export interface EditorSettings {
     fontSize: number;
 }
 
+export interface WindowSettings {
+    x?: number;
+    y?: number;
+    width: number;
+    height: number;
+    isMaximized: boolean;
+}
+
+export interface PanelSettings {
+    sidebarWidth: number;
+    fileExplorerWidth: number;
+    taskInfoWidth: number;
+}
+
+export interface LayoutSettings {
+    window: WindowSettings;
+    panels: PanelSettings;
+}
+
 export interface AppSettings {
     general: GeneralSettings;
     terminal: TerminalSettings;
     editor: EditorSettings;
+    layout: LayoutSettings;
 }
 
 export interface SettingsUpdatePayload {
     general?: Partial<GeneralSettings>;
     terminal?: Partial<TerminalSettings>;
     editor?: Partial<EditorSettings>;
+    layout?: {
+        window?: Partial<WindowSettings>;
+        panels?: Partial<PanelSettings>;
+    };
 }
